9cd84ee78d3a92d9a3096b9379b18f2dd77a84db
LauraIu <iuga.laura.katalin.com>
2022-11-10 19:03:09 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
 import ContactPage from "./pages/contact";
-import './App.css';
+import "./app.css";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="contact" element={<ContactPage />} />
-      </Routes>
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="contact" element={<ContactPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
