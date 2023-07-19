3ce0cac9b7ab72016dc45a627a8606f90be63734
Iuga <iuga.laura.katalin@gmail.com>
2023-07-19 11:54:02 +0200

    Updates

---
+++ precache-manifest.bd5e5ee787101a381ae3ebf1936c81ce.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "18dcc93e013bf0b17a73",
+    "url": "/portfolio/static/css/main.75067e4f.chunk.css"
+  },
+  {
+    "revision": "18dcc93e013bf0b17a73",
+    "url": "/portfolio/static/js/main.18dcc93e.chunk.js"
+  },
+  {
+    "revision": "43ef9566db297e50cc9c",
+    "url": "/portfolio/static/js/1.43ef9566.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "fa85d41f749b0a7ab4ddb27dc5ed8558",
+    "url": "/portfolio/static/media/webworks.fa85d41f.png"
+  },
+  {
+    "revision": "f2c7fa0057fd9e1f33099971a60a622b",
+    "url": "/portfolio/static/media/OtherWorks_IuLa.f2c7fa00.png"
+  },
+  {
+    "revision": "535d1f022613fada4cc43c49646e4c6b",
+    "url": "/portfolio/static/media/acacia_hostel.535d1f02.png"
+  },
+  {
+    "revision": "5d9573957ebe6f40581a06b18da3a335",
+    "url": "/portfolio/static/media/civil_radio_archivum.5d957395.png"
+  },
+  {
+    "revision": "e82c5e6386440b843c577a4fdb804ac0",
+    "url": "/portfolio/static/media/portfolio_iula.e82c5e63.png"
+  },
+  {
+    "revision": "7a5e5de61042b9f91399b8d9f318cb66",
+    "url": "/portfolio/static/media/Book_a_Table.7a5e5de6.png"
+  },
+  {
+    "revision": "28b9145fbb6356e5d52f84bfa64de224",
+    "url": "/portfolio/static/media/Social_Media_Dashboard_desktop1440w_dark_theme.28b9145f.png"
+  },
+  {
+    "revision": "2180a691136089ecbc026153bd983a52",
+    "url": "/portfolio/static/media/interactive_rating_component.2180a691.png"
+  },
+  {
+    "revision": "2201f2e2905359fcca38f04cf31a5ea1",
+    "url": "/portfolio/static/media/Iuga_Laura_portrait_04.2201f2e2.png"
+  },
+  {
+    "revision": "14c41b5fd455072996face13a9b7601d",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.59566a1539bc094512e25affdc8e902e.js"
+  "/portfolio/precache-manifest.bd5e5ee787101a381ae3ebf1936c81ce.js"
 );
 
 workbox.clientsClaim();
